be78ed0e0b85b4c14824dc34b87afb71994e655c
bart115 <120222886+bart115@users.noreply.github.com>
2024-05-28 17:11:55 +0100

    passos

--- sql/index.sql
+++ sql/index.sql
@@ -1,6 +1,9 @@
-
 -- Índice para consulta "Detetives" dá o id do detetive
 CREATE INDEX idx_detetive ON Detetive (idDetetive);
 
 -- Índice para consulta "Caso"
 CREATE INDEX idx_caso ON Caso (idCaso);
+
+-- Índice para consulta "Cliente"
+CREATE INDEX idx_cliente ON Cliente (NIF);
+
